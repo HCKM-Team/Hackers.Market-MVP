d882dd4a4968ce3a31b73e17a4d92e77c7970818
kevo <akerimberdi@gmail.com>
2025-08-02 09:45:49 +0300

    add etherlink config

--- frontend/src/lib/web3/wagmi.ts
+++ frontend/src/lib/web3/wagmi.ts
@@ -1,4 +1,5 @@
 import "@rainbow-me/rainbowkit/styles.css";
+
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import {
   arbitrum,
@@ -9,6 +10,28 @@ import {
   sepolia,
 } from "wagmi/chains";
 
+// Custom Etherlink chain configuration
+const etherlink = {
+  id: 42793,
+  name: "Etherlink",
+  network: "etherlink",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Tez",
+    symbol: "XTZ",
+  },
+  rpcUrls: {
+    default: { http: [process.env.NEXT_PUBLIC_ETHERLINK_RPC!] },
+    public: { http: [process.env.NEXT_PUBLIC_ETHERLINK_RPC!] },
+  },
+  blockExplorers: {
+    default: {
+      name: "Etherlink Explorer",
+      url: "https://explorer.etherlink.com",
+    },
+  },
+};
+
 export const config = getDefaultConfig({
   appName: "Hackers.Market",
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
@@ -18,6 +41,7 @@ export const config = getDefaultConfig({
     optimism,
     arbitrum,
     base,
+    etherlink,
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [sepolia] : []),
   ],
   ssr: true,
